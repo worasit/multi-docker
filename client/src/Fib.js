e781895440441450e3424171de0eab98f2fefd73
worasit daimongkol <worasit.dmk501@gmail.com>
2018-12-23 10:15:08 +0700

    fix error code

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -65,7 +65,7 @@ class Fib extends Component {
         </form>
 
         <h3>Indexes I have seen:</h3>
-        {this.renderSeenIndexes()}
+        {/*{this.renderSeenIndexes()}*/}
 
         <h3>Calculated Values:</h3>
         {this.renderValues()}
